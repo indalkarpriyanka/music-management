0ee28d30870acffb9cae05ba86a104fad8fe93f7
Priyanka <indalkar.priyanka@gmail.com>
2022-02-16 01:29:52 +0100

    detail screen updated

--- MusicManagement/app/src/main/java/com/appsfactory/musicmgmt/view/fragments/AlbumDetailsFragment.kt
+++ MusicManagement/app/src/main/java/com/appsfactory/musicmgmt/view/fragments/AlbumDetailsFragment.kt
@@ -1,16 +1,17 @@
 package com.appsfactory.musicmgmt.view.fragments
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import coil.load
 import com.appsfactory.musicmgmt.common.ResultModel
-import com.appsfactory.musicmgmt.common.utils.Constants
 import com.appsfactory.musicmgmt.data.remote.network.models.albumDetailsModels.AlbumDetailResponseModel
 import com.appsfactory.musicmgmt.databinding.FragmentAlbumDetailsBinding
 import com.appsfactory.musicmgmt.presentation.MainActivity
@@ -19,7 +20,10 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
+
 class AlbumDetailsFragment : Fragment() {
+
+    var thiscontext: Context? = null
 
     private lateinit var viewModel: AlbumDetailsViewModel
     private lateinit var albumResponseModel: AlbumDetailResponseModel
@@ -35,6 +39,7 @@ class AlbumDetailsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        thiscontext = container?.context
         binding = FragmentAlbumDetailsBinding.inflate(inflater, container, false)
         return binding?.root
     }
@@ -55,11 +60,34 @@ class AlbumDetailsFragment : Fragment() {
                     binding?.pgBar?.visibility =
                         View.GONE
                     albumResponseModel = it.data!!
-                    Log.d("url====>", Constants.formatUrl(it.data.album.image[0].toString()))
-                    binding?.imgAlbum?.load(it.data.album.image[1].toString())
-                    binding?.txtArtistNameValue?.text = it.data.album.artist
-                    (activity as AppCompatActivity?)!!.supportActionBar!!.title =
-                        it.data.album.name
+
+                    it.data.album.let { album ->
+                        Log.d("url====>", album.image[1].text.toString())
+                        binding?.imgAlbum?.load(album.image[1].text)
+                        binding?.txtAlbumNameValue?.text = album.name
+                        binding?.txtArtistNameValue?.text = album.artist
+                        /*(activity as AppCompatActivity?)!!.supportActionBar!!.title =
+                            album.name*/
+
+
+                        val N = album.tracks.track.size // total number of textviews to add
+                        val myTextViews = arrayOfNulls<TextView>(N) // create an empty array;
+                        for (i in 0 until N) {
+                            // create a new textview
+                            val rowTextView = TextView(thiscontext)
+
+                            // set some properties of rowTextView or something
+                            rowTextView.text = album.tracks.track[i].name
+
+                            // add the textview to the linearlayout
+                            binding?.llTrackNames?.addView(rowTextView)
+
+                            // save a reference to the textview for later
+                            myTextViews[i] = rowTextView
+                        }
+
+
+                    }
                 }
                 is ResultModel.Error -> {
                     binding?.pgBar?.visibility =
@@ -76,7 +104,7 @@ class AlbumDetailsFragment : Fragment() {
 
         binding?.btnFavourite?.setOnClickListener {
             CoroutineScope(Dispatchers.IO).launch {
-               // viewModel.addAlbumToDatabase(albumResponseModel.album.toAlbumEntity())
+                // viewModel.addAlbumToDatabase(albumResponseModel.album.toAlbumEntity())
             }
         }
     }
